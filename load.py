29b7bff3f187907231306951e993d54a80c2cf6a
zacharykirby <zkirby@live.com>
2017-07-13 08:44:36 -0500

    Added comments
    Helps clarify the code some more, maybe too much

--- load.py
+++ load.py
@@ -6,12 +6,12 @@ import sys
 import tarfile
 from IPython.display import display, Image
 from scipy import ndimage
-from sklearn.linear_model import LogisticRegression
-from six.moves.urllib.request import urlretrieve
+##from sklearn.linear_model import LogisticRegression
+##from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
 
 
-#set variables
+#set global variables
 img_size = 256
 pixel_depth = 255.0
 train_folders = []
@@ -19,25 +19,44 @@ test_folders = []
 valid_folders = []
 path = os.getcwd()
 
+#Define function to seperate the animation folders into training batches
+#PARAMS: None
+#RETURNS: None
 def create_folders():
+    
+    #This will be our training batch
     for x in range(4000):
         os.chdir(path + '\\' + str(x))
         train_folders.append(os.getcwd())
         #print(os.getcwd() + '- loaded')
+
+    #This will be our testing batch
     for x in range(4000,4500):
         os.chdir(path + '\\' + str(x))
         test_folders.append(os.getcwd())
         #print(os.getcwd() + '- loaded')
+
+    #This will be our validation batch
     for x in range(4500,5000):
         os.chdir(path + '\\' + str(x))
         valid_folders.append(os.getcwd())
         #print(os.getcwd() + '- loaded')
 
+
+#Define function to load animation folders into 3D arrays
+#PARAMS: Folder of animation containing frames
+#RETURNS: 3D ndarray with the animation data
 def load_anim(folder):
+    
+    #frame_files is a list of all the frame file names
     frame_files = os.listdir(folder)
+    #initialize dataset for loading and return
     dataset = np.ndarray(shape=(len(frame_files),img_size,img_size),
                          dtype=np.float32)
-    print(folder)
+    #print(folder)
+
+    #Loop to read in frame image data into an array, normalize it, then pass it
+    #to the dataset
     num_frames = 0
     for frame in frame_files:
         frame_file = os.path.join(folder,frame)
@@ -51,29 +70,41 @@ def load_anim(folder):
             num_frames += 1
         except IOError as e:
             print('Could not read:',frame_file,':',e,'- skipping')
+
+    #debugging statements
     dataset = dataset[0:num_frames, :, :]
     print('Full dataset tensor:', dataset.shape)
     print('Mean:', np.mean(dataset))
     print('Standard deviation:', np.std(dataset))
     return dataset
 
+#Define function to serialize the dataset created
+#PARAMS: List of folders to be serialized
+#RETURNS: list of the dataset names for testing
 def maybe_pickle(folders,force=False):
+    
     dataset_names = []
+
+    #loop to check if the folder has been pickled or not
     for folder in folders:
         set_filename = folder + '.pickle'
         dataset_names.append(set_filename)
         if os.path.exists(set_filename) and not force:
+            #pickle exists, skip it
             print('%s present - Skipping...' % set_filename)
         else:
+            #pickle must be created, load the animation
             print('Pickling %s.' % set_filename)
             dataset = load_anim(folder)
             try:
+                #dump data to pickle
                 with open(set_filename, 'wb') as f:
                     pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
             except Exception as e:
                 print('Unable to save to',set_filename,':',e)
     return dataset_names
 
+#Create the training, test, and validation folder lists
 create_folders()
 ##print(train_folders)
 ##print(test_folders)
